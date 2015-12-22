0b7eedca21e68067cb5594ec8b343d9a80fc0082
Donna Glinski <dglinski05@comcast.net>
2015-12-22 14:46:45 -0500

    adding Donna's computer

--- 00config.R
+++ 00config.R
@@ -22,6 +22,14 @@ if(Sys.info()[4]=="d2626utpurucke3.aa.ad.epa.gov"){
   micro.tables <- path.expand("~/git/glinski_metabolites/tables/")
 }
 
+if(Sys.info()[4]=="DONNA-PC"){
+  micro.root <- path.expand("C:/Users/Donna/Documents/Github/glinski_metabolites/")
+  micro.csv.in <- path.expand("C:/Users/Donna/Documents/Github/glinski_metabolites/csv_in/")
+  micro.csv.out <- path.expand("C:/Users/Donna/Documents/Github/glinski_metabolites/csv_out/")
+  micro.graphics <- path.expand("C:/Users/Donna/Documents/Github/glinski_metabolites/graphics/")
+  micro.tables <- path.expand("C:/Users/Donna/Documents/Github/glinski_metabolites/tables/")
+}
+
 if(Sys.info()[4]=="stp-air-3.local"){
   micro.root <- path.expand("~/git/glinski_metabolites/")
   micro.csv.in <- path.expand("~/git/glinski_metabolites/csv_in/")
